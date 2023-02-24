df2b0de0697fe0de1357e999d88a1816c9d3c1dc
CropsW <3501202786@qq.com>
2023-02-24 21:06:24 +0800

    编写设置窗口界面（待写槽逻辑）

--- settingsdialog.cpp
+++ settingsdialog.cpp
@@ -1,14 +1,22 @@
 #include "settingsdialog.h"
+
 #include "ui_settingsdialog.h"
 
-SettingsDialog::SettingsDialog(QWidget *parent) :
-  QDialog(parent),
-  ui(new Ui::SettingsDialog)
-{
+SettingsDialog::SettingsDialog(QWidget *parent)
+    : QDialog(parent), ui(new Ui::SettingsDialog) {
   ui->setupUi(this);
+  ui->lineEdit_potplayerExcutable->setText(
+      settings.value("potplayerExcutable").toString());
 }
 
-SettingsDialog::~SettingsDialog()
+SettingsDialog::~SettingsDialog() { delete ui; }
+
+void SettingsDialog::selectPotplayerExcutable()
 {
-  delete ui;
+
+}
+
+void SettingsDialog::closeEvent(QCloseEvent *) {
+  settings.setValue("potplayerExcutable",
+                    ui->lineEdit_potplayerExcutable->text());
 }
--- settingsdialog.h
+++ settingsdialog.h
@@ -1,22 +1,32 @@
 #ifndef SETTINGSDIALOG_H
 #define SETTINGSDIALOG_H
 
+#include <QCloseEvent>
+#include <QCoreApplication>
 #include <QDialog>
+#include <QFile>
+#include <QMessageBox>
+#include <QSettings>
 
 namespace Ui {
-  class SettingsDialog;
+class SettingsDialog;
 }
 
-class SettingsDialog : public QDialog
-{
+class SettingsDialog : public QDialog {
   Q_OBJECT
 
-public:
+ public:
   explicit SettingsDialog(QWidget *parent = nullptr);
   ~SettingsDialog();
 
-private:
+ private:
   Ui::SettingsDialog *ui;
+  QSettings settings = QSettings("settings.ini", QSettings::Format::IniFormat);
+ private slots:
+  void selectPotplayerExcutable();
+
+ protected:
+  void closeEvent(QCloseEvent *);
 };
 
-#endif // SETTINGSDIALOG_H
+#endif  // SETTINGSDIALOG_H
